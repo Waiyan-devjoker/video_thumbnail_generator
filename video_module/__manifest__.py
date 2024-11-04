58f455d1cf09860bf991b6cec6cd5f3f19783cde
waiyandevjoker <waiyan.devjoker@gmail.ocm>
2024-11-05 01:05:28 +0700

    push add module

---
+++ video_module/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Video Module',
+    'version': '18.0.1.0.0',
+    'summary': 'Video Thumbnail Generator',
+    'description': 'Video Thumbnail Generator',
+    'category': 'Extra Tools',
+    'author': 'WaiYan-DEVJ@K3R, FourLeafHouse',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+
+    ],
+    'installable': True,
+    'auto_install': False
+}
